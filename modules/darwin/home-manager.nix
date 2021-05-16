e556a32fba1186b8e8e3bf549b40d5b06216eb81
Russell Sherman <russell.t.sherman@gmail.com>
2021-05-16 14:50:42 -0500

    move user packages into separate file

--- modules/darwin/home-manager.nix
+++ modules/darwin/home-manager.nix
@@ -11,54 +11,7 @@
         enableDebugInfo = true;
         extraOutputsToInstall = [ "doc" "info" "devdoc" ];
         file = {};
-        packages = with pkgs; [
-          _1password
-          argocd
-          awscli2
-          awslogs
-          errcheck
-          go
-          go-outline
-          go-symbols
-          goa
-          gocode
-          gocode-gomod
-          gocyclo
-          godef
-          goimports
-          golangci-lint
-          golint
-          gomodifytags
-          gopkgs
-          gopls
-          gotags
-          gotools
-          gox
-          helmfile
-          k9s
-          kind
-          kube-prompt
-          kubecfg
-          kubectl
-          kubectx
-          kubernetes-helm
-          kubetail
-          kubeval
-          kustomize
-          lazydocker
-          lazygit
-          minikube
-          open-policy-agent
-          python37
-          python37Packages.pip
-          python37Packages.python-language-server
-          python37Packages.virtualenv
-          skaffold
-          tasksh
-          wakatime
-          watchman
-          youtube-dl
-        ];
+        packages = (import ./user-packages.nix { inherit pkgs; });
         sessionVariables = {
           PAGER = "less -R";
           EDITOR = "emacsclient";
---
+++ modules/darwin/user-packages.nix
@@ -0,0 +1,51 @@
+{ pkgs, ... }:
+
+with pkgs; [ 
+  _1password
+  argocd
+  awscli2
+  awslogs
+  errcheck
+  go
+  go-outline
+  go-symbols
+  goa
+  gocode
+  gocode-gomod
+  gocyclo
+  godef
+  goimports
+  golangci-lint
+  golint
+  gomodifytags
+  gomplate
+  gopkgs
+  gopls
+  gotags
+  gotools
+  gox
+  helmfile
+  k9s
+  kind
+  kube-prompt
+  kubecfg
+  kubectl
+  kubectx
+  kubernetes-helm
+  kubetail
+  kubeval
+  kustomize
+  lazydocker
+  lazygit
+  minikube
+  open-policy-agent
+  python37
+  python37Packages.pip
+  python37Packages.python-language-server
+  python37Packages.virtualenv
+  skaffold
+  tasksh
+  wakatime
+  watchman
+  youtube-dl
+]
